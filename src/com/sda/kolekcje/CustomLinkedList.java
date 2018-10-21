52bacd268b1302ece5f7b2ed81c6a07f70723654
Meandester <filipsek@live.com>
2018-10-21 15:16:21 +0200

    wiedza z zajec

--- src/com/sda/kolekcje/CustomLinkedList.java
+++ src/com/sda/kolekcje/CustomLinkedList.java
@@ -1,18 +1,82 @@
 package com.sda.kolekcje;
 
-public class CustomLinkedList implements CustomList {
-    @Override
-    public Object get(int index) {
-        return null;
+public class CustomLinkedList<E> implements CustomList<E> {
+
+    private int size;
+    private Node head;
+
+    public CustomLinkedList() {
+        head = new Node(null);
     }
 
     @Override
-    public void add(Object o) {
+    public void addTail(E e) {
+        Node tmp = new Node(e);
+        while (tmp.next != null){
+            tmp = tmp.next;
+        }
+        tmp.next = new Node(e);
+        System.out.println(tmp.next);
+    }
 
+    @Override
+    public void find(int index) {
+        Node tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        System.out.println(tmp.data);
+    }
+
+    @Override
+    public void add(E e) {
+        Node copy = head;
+        System.out.println("Wartość copy: " + copy);
+        head = new Node(e);
+        System.out.println("Wartość head: " + head);
+        head.next = copy;
+        System.out.println("Wartość next: " + head.next);
+        size++;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
     }
 
     @Override
     public int size() {
         return 0;
+    }
+
+    public void printList() {
+        Node tmp = head;
+        while (tmp != null) {
+            System.out.println(tmp.data);
+            tmp = tmp.next;
+        }
+    }
+
+
+    class Node {
+        private Object data;
+        private Node next;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+
+        public Object getData() {
+            return this.data;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "data=" + data +
+                    ", next=" + next +
+                    '}';
+        }
+
     }
 }
--- src/com/sda/kolekcje/Main.java
+++ src/com/sda/kolekcje/Main.java
@@ -8,19 +8,48 @@ public class Main {
     public static void main(String[] args) {
         // Array list jest implementacją tabicową
         // Linked list jest implementacja wiazana
-        List<String> list = new LinkedList<>();
 
-        list.add("Jan");
-        list.add("Tomek");
-        list.add("Marek");
-        list.add("Zdzichu");
-        list.add("Szymon");
-        list.add("Wojtek");
-        list.add("Slawomir");
+        CustomList<String> customList = new CustomLinkedList<>();
 
-        for (String l: list){
-            System.out.println(l);
+        customList.add("Jeden");
+        customList.add("Dwa");
+        customList.add("Trzy");
+        customList.add("Cztery");
+        customList.add("Piec");
+        customList.add("Szesc");
+
+        customList.addTail("Szesc");
+        customList.addTail("Piec");
+        customList.addTail("Cztery");
+        customList.addTail("Trzy");
+        customList.addTail("Dwa");
+        customList.addTail("Jeden");
+
+        /**
+        for (int i = 0; i < customList.size(); i++) {
+            System.out.println(customList.get(i));
         }
+         */
+
+        customList.printList();
+
+        customList.find(5);
 
     }
+    /**
+     List<String> list = new LinkedList<>();
+
+     list.add("Jan");
+     list.add("Tomek");
+     list.add("Marek");
+     list.add("Zdzichu");
+     list.add("Szymon");
+     list.add("Wojtek");
+     list.add("Slawomir");
+
+     for (String l: list){
+     System.out.println(l);
+     }
+     */
+
 }
