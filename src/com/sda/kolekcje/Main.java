4b6a16741ae611b3ab335f1950e98e9fb33746da
Meandester <filipsek@live.com>
2018-10-21 13:39:04 +0200

    dodano listy

--- src/com/sda/kolekcje/Main.java
+++ src/com/sda/kolekcje/Main.java
@@ -1,8 +1,26 @@
 package com.sda.kolekcje;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Array list jest implementacją tabicową
+        // Linked list jest implementacja wiazana
+        List<String> list = new LinkedList<>();
+
+        list.add("Jan");
+        list.add("Tomek");
+        list.add("Marek");
+        list.add("Zdzichu");
+        list.add("Szymon");
+        list.add("Wojtek");
+        list.add("Slawomir");
+
+        for (String l: list){
+            System.out.println(l);
+        }
+
     }
 }
